f52b9506105358f1e0751a0f82fddc7fb7d9cb34
Oleg G.Kapranov <ok@pure-agency.co>
2025-09-29 21:41:44 -1000

    updated README.md and added start.sh

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 nfc_card
 =====
 
+```
+rebar3 new release nfc_card
+```
+
 ### 30 Sep 2025 by Oleg G.Kapranov
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+export ERL_CRASH_DUMP_SECONDS=1
+
+erl \
+  -name session1@axion.local \
+  -pa ebin deps/*/ebin \
+  -config config/sys.config \
+  -eval 'lists:foreach(fun(App) -> ok = application:start(App) end, [])'
