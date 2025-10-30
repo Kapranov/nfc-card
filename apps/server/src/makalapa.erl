278800bd51509a3735dd5436f627ff4db9e29b82
Oleg G.Kapranov <ok@pure-agency.co>
2025-10-29 23:46:03 -1000

    updated makalapa.erl

--- apps/server/src/makalapa.erl
+++ apps/server/src/makalapa.erl
@@ -1,6 +1,6 @@
 -module(makalapa).
 
--export([amqp_params/0,consumer_pid/3,handle_message/0,read_messages/2,send_message/3,setup_consumer/2,start/3]).
+-export([amqp_lifecycle/0,amqp_params/0,consumer_pid/3,handle_message/0,init_consumer/2,read_messages/1,read_messages/2,send_message/3,send_message/4,setup_consumer/2,start/3]).
 
 -include("./_build/default/lib/amqp_client/include/amqp_client.hrl").
 
@@ -47,6 +47,32 @@ handle_message() ->
       io:format("Received message: ~p~n", [binary_to_list(Payload)])
   end.
 
+read_messages(Timeouts) ->
+  receive
+    {#'basic.deliver'{consumer_tag=_ConsumerTag, delivery_tag=_DeliveryTag, redelivered=_Redelivered, exchange=_Exchange, routing_key=RoutingKey}, Content} ->
+      log(read_messages,"basic.deliver"),
+      io:format("RoutingKey received: ~p~n", [RoutingKey]),
+      #amqp_msg{payload = Payload} = Content,
+      io:format("Payload received: ~p~n", [Payload]),
+      read_messages(0);
+    Any ->
+      io:format("received unexpected Any: ~p~n", [Any]),
+      read_messages(0)
+  after 1000 ->
+    case Timeouts of
+      0 ->
+        Timeouts2 = Timeouts + 1,
+        read_messages(Timeouts2);
+      5 ->
+        io:format("~n"),
+        io:format("Message timeout exceeded ~n");
+      _ ->
+        Timeouts2 = Timeouts + 1,
+        io:format("."),
+        read_messages(Timeouts2)
+    end
+  end.
+
 read_messages(Queue, Timeouts) ->
   receive
     {#'basic.deliver'{consumer_tag=_ConsumerTag, delivery_tag=_DeliveryTag, redelivered=_Redelivered, exchange=_Exchange, routing_key=_RoutingKey}, Content} ->
@@ -79,6 +105,12 @@ send_message(Queue, Exchange, Msg) ->
   amqp_connection:close(Conn),
   ok.
 
+send_message(Channel, Exchange, RoutingKey, Payload) ->
+    log(send_message,"basic.publish setup"),
+    BasicPublish = #'basic.publish'{exchange = Exchange, routing_key = RoutingKey},
+    log(send_message,"amqp_channel:cast"),
+    ok = amqp_channel:cast(Channel, BasicPublish, _MsgPayload = #amqp_msg{payload = Payload}).
+
 setup_consumer(Queue, ConsumerName) ->
   {ok, Conn} = amqp_connection:start(amqp_params()),
   {ok, Channel} = amqp_connection:open_channel(Conn),
@@ -105,4 +137,48 @@ start(Queue, Exchange, Type) ->
   #'queue.bind_ok'{} = amqp_channel:call(Channel, #'queue.bind'{queue = binary(Queue), exchange = binary(Exchange)}),
   amqp_channel:close(Channel),
   amqp_connection:close(Conn),
+  ok.
+
+init_consumer(Channel, Queue) ->
+  log(setup_consumer,"basic.consume"),
+  BasicConsume = #'basic.consume'{queue = Queue, consumer_tag = <<"">>, no_ack = true},
+  #'basic.consume_ok'{consumer_tag = ConsumerTag} = amqp_channel:subscribe(Channel, BasicConsume, self()),
+  log(setup_consumer,"basic.consume_ok start receive"),
+  receive
+    #'basic.consume_ok'{consumer_tag = ConsumerTag} -> ok
+  end,
+  log(setup_consumer,"basic.consume_ok finished"),
+  log(read_messages,"start"),
+  Msg = read_messages(0),
+  io:format("Msg: ~p~n", [Msg]),
+  log(read_messages,"finish"),
+  log(basic_cancel,"start"),
+  BasicCancel = #'basic.cancel'{consumer_tag = ConsumerTag},
+  #'basic.cancel_ok'{consumer_tag = ConsumerTag} = amqp_channel:call(Channel,BasicCancel).
+
+amqp_lifecycle() ->
+  {ok, Connection} = amqp_connection:start(amqp_params()),
+  {ok, Channel} = amqp_connection:open_channel(Connection),
+  BindKey = <<"#">>,
+  QueueDeclare = #'queue.declare'{queue = ?EVENTS_QUEUES},
+  #'queue.declare_ok'{queue = Queue, message_count = MessageCount, consumer_count = ConsumerCount} = amqp_channel:call(Channel, QueueDeclare),
+  log(queue,Queue),
+  log(message_count,MessageCount),
+  log(consumer_count,ConsumerCount),
+  ExchangeDeclare = #'exchange.declare'{exchange = ?EVENTS_EXCHANGE, type = <<"topic">>},
+  #'exchange.declare_ok'{} = amqp_channel:call(Channel, ExchangeDeclare),
+  QueueBind = #'queue.bind'{queue = Queue, exchange = ?EVENTS_EXCHANGE, routing_key = BindKey},
+  #'queue.bind_ok'{} = amqp_channel:call(Channel, QueueBind),
+  log(send_message,"start"),
+  RoutingKey = <<"test.routing.key">>,
+  Payload = <<"This is a really interesting message!">>,
+  send_message(Channel, ?EVENTS_EXCHANGE, RoutingKey, Payload),
+  log(setup_consumer,"start"),
+  init_consumer(Channel, Queue),
+  log(setup_consumer,"finished"),
+  log(channel_close,"start"),
+  ok = amqp_channel:close(Channel),
+  log(connection_close,"start"),
+  ok = amqp_connection:close(Connection),
+  log(connection_close,"Demo Completed!"),
   ok.
